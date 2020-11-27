9cbe13512bc77f55b7e7ec09fcc68091be8a7aa7
Yeshuya <55270550+Yeshuya@users.noreply.github.com>
2020-11-27 15:58:55 +0800

    Create canvas.php

---
+++ canvas.php
@@ -0,0 +1,71 @@
+<style>
+    .container_canvas{
+        margin-left: 378px;
+        font-family: "Staatliches";
+    }
+
+    .content_canvas{
+        width: 100%;
+        min-height: 500px;
+    }
+    /* Dropdown Button */
+    .dropbtn {
+        background-color: #183c64;
+        color: white;
+        padding: 16px;
+        font-size: 25px;
+        border: none;
+        width: 250px;
+        font-family: "Staatliches";
+    }
+
+    /* The container <div> - needed to position the dropdown content */
+    .dropdown {
+        position: relative;
+        display: inline-block;
+        margin-top: 50px;
+        margin-left: 5px;
+        font-family: "Staatliches";
+    }
+
+    /* Dropdown Content (Hidden by Default) */
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #183c64;
+        min-width: 250px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+    /* Links inside the dropdown */
+    .dropdown-content a {
+        color: white;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+        font-family: "Staatliches";
+    }
+
+    /* Change color of dropdown links on hover */
+    .dropdown-content a:hover {background-color: #183c64;}
+
+    /* Show the dropdown menu on hover */
+    .dropdown:hover .dropdown-content {display: block;}
+
+    /* Change the background color of the dropdown button when the dropdown content is shown */
+    .dropdown:hover .dropbtn {background-color: white; color: #183c64;}
+</style>
+
+<div class="container_canvas">
+	  <div class="dropdown">
+        <button class="dropbtn">Module 7</button>
+        <div class="dropdown-content">
+            <a href="Canvas/index.php"> Learning Task 7.1 </a>	
+        </div>
+    </div>
+</div>
+
+<div class="content_canvas">
+	
+</div>
